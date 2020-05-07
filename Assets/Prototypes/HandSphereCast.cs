9c363952f66906e6f1f04b35fb5e7b457ab85867
inventonater <thedenrei@gmail.com>
2020-05-07 16:10:11 -0700

    HandSphereCast creates its own GameObjects

--- Assets/Prototypes/HandSphereCast.cs
+++ Assets/Prototypes/HandSphereCast.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class HandSphereCast : MonoBehaviour
 {
-    public GameObject Sphere;
+    public GameObject _sphereCast;
     public float _radius;
     public Rigidbody _target;
     public float _pullForce = 75f;
@@ -15,13 +15,25 @@ public class HandSphereCast : MonoBehaviour
     private void Awake()
     {
         _hand = FindRightHand();
+        
+        if (!_target)
+        {
+            _target = GameObject.CreatePrimitive(PrimitiveType.Sphere).AddComponent<Rigidbody>();
+            _target.transform.localScale = Vector3.one * 2;
+        }
+
+        if (!_sphereCast)
+        {
+            _sphereCast = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            _sphereCast.GetComponent<SphereCollider>().enabled = false;
+            _sphereCast.transform.localScale = Vector3.one * 2;
+        }
     }
 
     public static OVRHand FindRightHand()
     {
         return FindObjectsOfType<OVRHand>().First(h => h.transform.parent.name.Contains("Right"));
     }
-
     
     private void FixedUpdate()
     {
@@ -41,7 +53,7 @@ public class HandSphereCast : MonoBehaviour
         
         if(Physics.SphereCast(transform.position, _radius, pose.forward, out var hitInfo))
         {
-            Sphere.transform.position = hitInfo.point;
+            _sphereCast.transform.position = hitInfo.point;
             if (_target)
             {
                 var toHitPoint = Vector3.ClampMagnitude(hitInfo.point - _target.position, 10f);
