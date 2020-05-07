72ef9222ae41d206494b68ea1ceff2fa23f72647
inventonater <thedenrei@gmail.com>
2020-05-06 22:46:48 -0700

    Lots of prototyping

--- Assets/Prototypes/FingerToggle.cs
+++ Assets/Prototypes/FingerToggle.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class FingerToggle : MonoBehaviour
 {
     public GameObject PowerPrefab;
+    public OVRSkeleton.BoneId BoneId = OVRSkeleton.BoneId.Hand_IndexTip;
+    public OVRHand.HandFinger HandFinger = OVRHand.HandFinger.Index;
+    
     private GameObject _power;
-
     private OVRHand _hand;
     private OVRSkeleton _skeleton;
     private Vector3 _defaultScale = Vector3.one * 0.01f;
+    
     private void Awake()
     {
-        _hand = FindObjectsOfType<OVRHand>().First(h => h.transform.parent.name.Contains("Right"));
+        _hand = HandSphereCast.FindRightHand();
         _skeleton = _hand.GetComponent<OVRSkeleton>();
         _power = Instantiate(PowerPrefab);
         _power.transform.localScale = Vector3.zero;
@@ -30,8 +33,8 @@ public class FingerToggle : MonoBehaviour
         Velocity = (pose.position - transform.position) / Time.deltaTime;
         transform.SetPositionAndRotation(pose.position, pose.rotation);
 
-        _power.transform.localScale = _defaultScale * (_hand.GetFingerPinchStrength(OVRHand.HandFinger.Index)*3+1);
-        var indexTip = _skeleton.Bones.FirstOrDefault(b => b.Id == OVRSkeleton.BoneId.Hand_IndexTip);
+        _power.transform.localScale = _defaultScale * (_hand.GetFingerPinchStrength(HandFinger)*3+1);
+        var indexTip = _skeleton.Bones.FirstOrDefault(b => b.Id == BoneId);
         if (indexTip != default)
         {
             _power.transform.SetPositionAndRotation(indexTip.Transform.position, indexTip.Transform.rotation);
---
+++ Assets/Prototypes/HandSphereCast.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Linq;
+using UnityEngine;
+
+public class HandSphereCast : MonoBehaviour
+{
+    public GameObject Sphere;
+    public float _radius;
+    public Rigidbody _target;
+    public float _pullForce = 75f;
+    private OVRHand _hand;
+    private float _lastJump;
+    private Vector3 JumpForce = Vector3.up*500f;
+
+    private void Awake()
+    {
+        _hand = FindRightHand();
+    }
+
+    public static OVRHand FindRightHand()
+    {
+        return FindObjectsOfType<OVRHand>().First(h => h.transform.parent.name.Contains("Right"));
+    }
+
+    
+    private void FixedUpdate()
+    {
+        if (!_hand.IsTracked || !_hand.IsDataValid)
+        {
+            return;
+        };
+
+        var pose = _hand.PointerPose;
+        transform.SetPositionAndRotation(pose.position, pose.rotation);
+
+        if (Time.time-_lastJump > .5f && _hand.GetFingerIsPinching(OVRHand.HandFinger.Index))
+        {
+            _target.AddForce(JumpForce);
+            _lastJump = Time.time;
+        }
+        
+        if(Physics.SphereCast(transform.position, _radius, pose.forward, out var hitInfo))
+        {
+            Sphere.transform.position = hitInfo.point;
+            if (_target)
+            {
+                var toHitPoint = Vector3.ClampMagnitude(hitInfo.point - _target.position, 10f);
+                _target.AddForce(toHitPoint * _pullForce * Time.fixedDeltaTime);
+            }
+        }
+
+    }
+
+}
